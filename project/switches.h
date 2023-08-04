1dea19523c4991efe522a949fecef2017c08b3e7
agonzalez118 <113930443+agonzalez118@users.noreply.github.com>
2023-08-04 08:09:26 -0600

    Update switches.h

--- project/switches.h
+++ project/switches.h
@@ -1,6 +1,5 @@
 #ifndef switches_included
 #define switches_included
-
 #define SW1 BIT0
 #define SW2 BIT1
 #define SW3 BIT2
@@ -10,7 +9,6 @@
 void switch_init();
 unsigned char switch_input();
 //void switch_interrupt_handler();
-
 extern char switch_state_down, switch_state_changed; /* effectively boolean */
 
 #endif // included
