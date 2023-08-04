abd454b8734990ed6eaa514b105b117e697045b1
agonzalez118 <113930443+agonzalez118@users.noreply.github.com>
2023-08-04 07:52:15 -0600

    main.c

--- project/main.c
+++ project/main.c
@@ -1,1 +1,12 @@
-fb
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+#include "switches.h"
+
+void main(void) {
+  configureClocks();
+  switch_init();
+  led_init();
+
+  or_sr(0x18);
+}
